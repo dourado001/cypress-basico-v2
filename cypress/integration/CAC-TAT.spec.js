5a5d411696bcd43f802056fdbc211714b4fd3874
Guilherme Barbosa <guilherme.dourado95@gmail.com>
2024-02-01 14:25:37 -0300

    Subindo teste campos obrigatorios

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -7,10 +7,19 @@
 
 /// <reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+describe('Central de Atendimento ao Cliente TAT', function () {
+    beforeEach(() => {
         cy.visit('./src/index.html')
+    })
+    it('verifica o título da aplicação', function () {
         cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
     })
-  })
-  
+    it('preenche campos obrigatorios', function () {
+        cy.get('#firstName').type('Neymar')
+        cy.get('#lastName').type('Jr')
+        cy.get('#email').type('neymar.jr@fundacaoneymar.com')
+        cy.get('#open-text-area').type('Nao to gordo!')
+        cy.get('button[type="submit"]').click()
+        cy.get('span[class="success"]').should('be.visible')
+    })
+})
